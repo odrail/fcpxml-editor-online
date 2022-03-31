aa95047efc4e7f58f5abad785098d7ddaf68b5a8
Emanuele Liardo <emanuele.liardo@gullivernet.com>
2022-03-31 23:32:54 +0200

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,18 @@ import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import ReactGA from 'react-ga'
+import { useEffect } from 'react'
+
+// ReactGA.initialize('G-LQGNV7QMEV')
+ReactGA.initialize('UA-187959591-1')
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    console.log('pageview')
+    ReactGA.pageview(window.location.pathname)
+  })
+
   return (
     <Provider store={store}>
       <Head>
